989ca0a631957cf4773ed181d1cc4bc945fd038b
sahrissa.sullivan <ssulli25@live.spcollege.edu>
2024-11-10 13:06:26 -0500

    commit - m "Testing Commit"

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1ea45efec9cf64808e94b427c227bd9c95e45e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a0ea1c9184bd6e96443aa02eeb1d69f6122caa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
