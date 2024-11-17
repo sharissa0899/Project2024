2b3dc57929508bf681bb3b37602b5941c46924f7
sahrissa.sullivan <ssulli25@live.spcollege.edu>
2024-11-17 12:14:35 -0500

    Testing

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2260c37f3d57b1324c9472335388a253d94d3e09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c746db3dc3069ed073c887cd9a7609c962948fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
